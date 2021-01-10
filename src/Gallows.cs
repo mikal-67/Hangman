bd00c0a6da5beeb500273866d382f1ba05ca1b31
mikal-67 <72887058+mikal-67@users.noreply.github.com>
2021-01-10 12:54:04 -0600

    Add files via upload

---
+++ src/Gallows.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangmansharp
+{
+    /// <summary>
+    /// A class representing the gallows.
+    /// </summary>
+    public class Gallows
+    {
+        int value = 0;
+        public Gallows(int i)
+        {
+            value = i;
+        }
+        public void Display()
+        {
+            if (value == 0)
+            {
+                Console.WriteLine(" ====================");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+            }
+            else if (value == 1)
+            {
+                Console.WriteLine(" ====================");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  O");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+            }
+            else if (value == 2)
+            {
+                Console.WriteLine(" ====================");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  O");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+            }
+            else if (value == 3)
+            {
+                Console.WriteLine(" ====================");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  O");
+                Console.WriteLine(" |                 /|");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+            }
+            else if (value == 4)
+            {
+                Console.WriteLine(" ====================");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  O");
+                Console.WriteLine(" |                 /|\\");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+            }
+            else if (value == 5)
+            {
+                Console.WriteLine(" ====================");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  O");
+                Console.WriteLine(" |                 /|\\");
+                Console.WriteLine(" |                  /");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+            }
+            else if(value == 6)
+            {
+                Console.WriteLine(" ====================");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  |");
+                Console.WriteLine(" |                  O");
+                Console.WriteLine(" |                 /|\\");
+                Console.WriteLine(" |                  /\\");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+                Console.WriteLine(" |                   ");
+            }
+        }
+    }
+}
---
+++ src/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangmansharp
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Gallows g = new Gallows(1);
+            g.Display();
+            var r = new Random();
+            //get a random word from words.txt
+            string word = System.IO.File.ReadAllLines("words.txt")[r.Next(System.IO.File.ReadAllLines("words.txt").Length - 1)];
+            Console.WriteLine("Hangman version 1.0.0");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
